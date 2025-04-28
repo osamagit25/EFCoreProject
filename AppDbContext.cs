d4269292a0ae00338e1ebe2bf6ddaa2c9462c94a
Osama Radwan <osamamhmd24@outlook.com>
2025-04-28 17:51:40 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EFCoreProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreProject.Context
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentAffairs> StudentAffairs { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=OSAMARADWAN;DataBase=School;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+    }
+}
